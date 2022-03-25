7ce5b02589cbfa4e20a703f76a47a04f40781557
TINUOYE1 <64705485+TINUOYE1@users.noreply.github.com>
2022-03-25 06:17:16 -0500

    Add files via upload

--- app.component.ts
+++ app.component.ts
@@ -1,27 +1,54 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  project_status = ['stable', 'critical', 'finished']
 
-  defaultSub="Advance"
+  forbiddenNames = ['test'];
 
-  @ViewChild ('f') subForm: NgForm
+  testForbiddenNames = ['testForbidden'];
 
-  subFormTemplate = 
-  {email: "",
-  subscription: "",
-  password: ""}
-  submitted= false
-  onSubmit(){
-    console.log(this.subForm)
-    this.subFormTemplate.email = this.subForm.form.value.email;
-    this.subFormTemplate.subscription = this.subForm.form.value.subscription;
-    this.subFormTemplate.password = this.subForm.form.value.password;
-    this.submitted = true;
+  projectRegForm : FormGroup;
+
+  ngOnInit(): void {
+    this.projectRegForm = new FormGroup({
+      'projectName': new FormControl(null, [Validators.required, this.forbiddenProjectName.bind(this)], this.testForbiddenAsync),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl('stable', Validators.required)
+    })
+
+    
   }
+
+  forbiddenProjectName(control: FormControl): {[s: string] : boolean}{
+    if(this.forbiddenNames.indexOf(control.value) !== -1) {
+      return {['InvalidProjectName'] : true};
+    }
+    return null;
+    
+
+  }
+
+  testForbiddenAsync(control: FormControl): Promise<any> | Observable<any>{
+   const promise = new Promise ((resolve, reject) => {
+     setTimeout(() => {
+       if (control.value === 'testforbidden'){
+         resolve({['InvalidProjectName'] : true});
+       }else{
+         resolve(null)
+       }
+     }, 2000)
+   })
+   return promise;
+  }
+
+  onCreateProject(){
+    console.log(this.projectRegForm.value)
+  }
+    
 }
